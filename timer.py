0b775765a2c025032f670aa3298334e0ef7f6967
a-szto <hohinszeto@gmail.com>
2024-10-15 15:01:02 -0400

    Program displays "Players stand"

--- timer.py
+++ timer.py
@@ -12,6 +12,8 @@ timer.py uses the time library to help keep track of time
 import time # The time library has a sleep function that will pause the script for a specifized amount of time
 from PIL import Image # the pillow library makes it easy to display images 
 
+print("Players stand")
+
 im = Image.open("times-up.jpeg")
 
 # ask user to enter desired countdown time
